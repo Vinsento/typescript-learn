a56fab4eb32ee680a4ba13405d3225df1cbdedf3
Vinsento <hummanist@gmail.com>
2023-02-14 18:20:24 +0200

    button done, add svg (SVGR)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,21 @@
 import { ButtonProps } from "./Button.props";
 import styles from "./Button.module.scss";
+import Arrow from "./button_arrow.svg";
 
-export const Button = ({ children, appearance }: ButtonProps): JSX.Element => {
-    return <button className={`${styles.button} ${styles[appearance]}`}>{children}</button>
-}
+export const Button = ({
+  children,
+  appearance,
+  arrow = "none",
+  ...props
+}: ButtonProps): JSX.Element => {
+  return (
+    <button className={`${styles.button} ${styles[appearance]}`} {...props}>
+      {children}
+      {arrow !== "none" && (
+        <span className={`${styles.arrow} ${styles[arrow]}`}>
+          <Arrow />
+        </span>
+      )}
+    </button>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import { Title } from "@/components/Title/Title";
 import { Button } from "../components/Button/Button";
 
 export default function Home() {
-  return (<>
-      <Title tag="h1">Heading</Title>  
-      <Button appearance="secondary">Button</Button>
+  return (
+    <>
+      <Title tag="h1">Heading</Title>
+      <Button appearance="secondary" arrow="right">
+        Button
+      </Button>
     </>
-  )
+  );
 }
